17fb95762287a259a46a2f7f6b189b779b4ae848
Gaston Mollica <mollica.gaston@gmail.com>
2019-07-10 20:13:58 +1000

    nodeJS up and running

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+// This imports express in so it can be used
 var express = require('express');
+
+// This is the server object
+var server = express();
+
+var staticAssets = express.static('public');
+server.use(staticAssets);
+
+// Server is listening out for port 3000.
+server.listen(3000, function() {
+  console.log('Server had started listening on port 3000.');
+});
